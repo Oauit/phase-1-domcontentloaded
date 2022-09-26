c4091f25c1de29a729d1ed501ae871531712b58c
oauit <aidanjade.ward@gmail.com>
2022-09-26 17:08:48 -0600

    commit tree 213cf5031247ef2a0062fe1d9fa11509ba6871a5

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('The DOM has loaded');
+    let p = document.querySelector('p');
+    p.textContent = 'This is really cool!';
+    console.log(p);
+});
